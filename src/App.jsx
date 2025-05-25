4785c46ad704ced3bb905601a53e83e1c049c77d
Ayan Khurt <ayankhurt@gmail.com>
2025-05-25 16:00:44 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const baseUrl = 'https://backend-db-tau.vercel.app';
+  const baseUrl = 'https://backend-db-seven.vercel.app';
   // const baseUrl = 'http://localhost:3000';
 
   const getProducts = async () => {
